fdeb883dc52e611bf9527210ea30fcbb8a599ec5
Chris <christiangallegosd@gmail.com>
2017-12-11 19:44:55 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Change Calculator
+
+This project calculates the change due from a sale and displays the result in USD currency. 
+
+## Built With
+
+* Javascript
+* HTML
+* CSS
+
+## Getting Started
+
+### Installing
+
+Clone this repository by running the following command on terminal:
+
+`git clone https://github.com/larrycustodio/startnow-web101-change-calculator.git`
+
+Then download the project dependencies by running:
+
+`npm install`
+
+Finally, launch index.html in the terminal or simply open ./index.html in your browser of choice.
+
+### Testing
+
+To run the tests:
+
+`npm test`
---
+++ main.js
@@ -0,0 +1,47 @@
+function calculateChange(cost, tendered) {
+  // multiply any change due by 100 to reduce decimal calculations / errors when dealing with cents
+  let calculate = (tendered - cost).toFixed(2) * 100;
+  return calculate;
+}
+
+function handleClickEvent() {
+  // get the values from the input fields and pass into calculateChange method as arguments
+  let tendered = document.getElementById("amount-received").value;
+  let cost = document.getElementById("amount-due").value;
+  let changeDue = calculateChange(cost, tendered);
+
+  // dollars
+  if (changeDue >= 100) {
+    // convert dollars due back to a count of one. Repeat for each denomination. e.g., 400 === 4
+    let dollars = Math.floor(changeDue / 100);
+    // if there is change left after dollars are calculated, assign to changeDue variable for use in next denomination
+    changeDue %= 100;
+    // pass in the new value of dollars, if any, to the HTML page with the corresponding ID. Repeat for each denomination
+    document.getElementById("dollars-output").innerHTML = dollars;
+  }
+  // quarters
+  if (changeDue < 100 && changeDue >= 25) {
+    let quarters = Math.floor(changeDue / 25);
+    changeDue %= 25;
+    document.getElementById("dollars-output").innerHTML = '0';
+  }
+  // dimes
+  if (changeDue < 25 && changeDue >= 10) {
+    let dimes = Math.floor(changeDue / 10);
+    changeDue %= 10;
+    document.getElementById("quarters-output").innerHTML = '0';
+  }
+  // nickels
+  if (changeDue < 10 && changeDue >= 5) {
+    let nickels = Math.floor(changeDue / 5);
+    changeDue %= 5;
+    document.getElementById("dimes-output").innerHTML = '0';
+  }
+  // pennies
+  if (changeDue >= 1 && changeDue < 5) {
+    let pennies = Math.round(changeDue);
+    document.getElementById("pennies-output").innerHTML = pennies;
+  }
+}
+// assign an event listener to the corresponding HTML ID and pass it handleClickEvent as a callback function, which executes changeDue calculations and prints results to the screen
+document.getElementById("calculate-change").addEventListener("click", handleClickEvent);
